dbaed3f2d4a1da4c7d2c0b7b332e0c6a2cd0112f
Natalie <nataliech0409@gmail.com>
2023-06-01 08:00:35 +0300

    добавлен опыт

---
+++ docs/js/experience.js
@@ -0,0 +1,8 @@
+const experienceSpan = document.querySelector('.experience')
+const totalExperience = experienceSpan.querySelector('span')
+
+const today = new Date();
+
+console.log(today);
+
+console.log(totalExperience);
